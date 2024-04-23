a039f8f9cf2c3a6ac927c088e6e1529c6cbd3ae8
Arjay <princessallanahg@gmail.com>
2024-04-24 02:41:05 +0800

    up

--- Config.php
+++ Config.php
@@ -2,13 +2,13 @@
 
 $sname = 'localhost';
 
-$unmae= 'root';
-$password = '';
-$db_name = 'cart_db';
+//$unmae= 'root';
+//$password = '';
+//$db_name = 'cart_db';
 
-//$unmae= 'u232113837_';
-//$password = '?Op6pDIp';
-//$db_name = 'u232113837_card_db';
+$unmae= 'u232113837_card_db';
+$password = '@wmsuPHS1';
+$db_name = 'u232113837_card_db';
 
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
