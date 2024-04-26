217b13df5b8effc01508b2ca001db8b8e91e23b3
ecommercess <167793012+ecommercess@users.noreply.github.com>
2024-04-26 19:24:16 +0800

    Up

--- add_tocart.php
+++ add_tocart.php
@@ -12,17 +12,6 @@ if ($logged == false) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Add to cart</title>
-    <style>
-        .btn{
-            background-color: peru;
-            padding-left: 20px;
-        }
-        .btm-warning{
-            background-color: peru;
-            padding-left: 20px;
-
-        }
-        </style>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
