2b3f8bb2b8f0fff9ad302ddf08b28ad9fbbad4b8
ecommercess <167793012+ecommercess@users.noreply.github.com>
2024-04-26 19:18:52 +0800

    Up

--- add_tocart.php
+++ add_tocart.php
@@ -17,6 +17,8 @@ if ($logged == false) {
             background-color: peru;
             padding-left: 20px;
         }
+        a{background-color: peru;
+            padding-left: 20px;}
         </style>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
